08df231415ea7dbe33ed9421f05f5e73f888e0d6
Andrey Avdeev <a3avdeev@gmail.com>
2022-07-24 23:34:04 +0300

    add task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryItemList = createImageMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryItemList);
+
+function createImageMarkup(item) {
+  return item
+    .map(({ preview, original, description }) => {
+      return `<li>
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    </li>`;
+    })
+    .join('');
+}
+
+const lightboxImg = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
